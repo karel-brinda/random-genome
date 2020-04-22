2ea882de84672474e58727b3e825cf3f37111cad
Karel Břinda <karel.brinda@gmail.com>
2020-04-22 11:33:02 -0400

    Generate random nucleotides

--- src/random-genome.cpp
+++ src/random-genome.cpp
@@ -1,28 +1,31 @@
-#include <errno.h>
-#include <stdio.h>
-#include <zlib.h>
 #include <cstdlib>
-#include <cmath>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2) {
+    if (argc != 2)
+    {
         fprintf(stderr, "Usage: %s <lenght>\n", argv[0]);
         return 1;
     }
 
-    long long length = atol(argv[1]);
-	printf(">random_genome");
+    srand(42);
 
-	char c='~';
-	for (long long i=0; i<length; i++) {
-		if (i%1000==0) {
-			putchar('\n');
-		}
-		c='A';
-		putchar(c);
-	}
-	putchar('\n');
+    long long length = atol(argv[1]);
+    printf(">random_genome");
+
+    char c = '~';
+    char chars[] = "ACGT";
+    for (long long i = 0; i < length; i++)
+    {
+        if (i % 1000 == 0)
+        {
+            putchar('\n');
+        }
+        c = chars[rand() % 4];
+        putchar(c);
+    }
+    putchar('\n');
 
     return 0;
 }
